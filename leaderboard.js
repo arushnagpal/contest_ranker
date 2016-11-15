372636aa8072939a1a16c64de1032b1f0e867b70
Arush Nagpal <arushngpl16@gmail.com>
2016-11-15 23:02:30 +0530

    Created Leaderboard View

--- leaderboard.js
+++ leaderboard.js
@@ -3,20 +3,31 @@ var router = express.Router();
 var con=require("./database");
 module.exports = router;
 
-router.route('/')
+router.route('/contests/:pid')
   .get(function (req, res) {
-    con.query('SELECT * FROM contests',function(err,rows){
+    var pid=req.params.pid;
+    con.query('select userid,score from leaderboard where problemid="'+pid+'" order by score desc',function(err,rows){
          if(err) throw err;
 
             else     
-              res.render("leaderboard_contests", { title : "Choose a contest", data : rows, user_name: req.session.user_name});
+              res.render("leaderboard", { title : "Leaderboard", data : rows, user_name: req.session.user_name,problem:pid});
+            console.log(rows);
+        });
+  });
+router.route('/')
+  .get(function (req, res) {
+    con.query('select userid, SUM(score) as score from leaderboard group by userid order by score DESC',function(err,rows){
+         if(err) throw err;
+
+            else     
+              res.render("leaderboard", { title : "Leaderboard", data : rows, user_name: req.session.user_name});
             console.log(rows);
         });
   });
 router.route('/:cid')
   .get(function (req, res) {
   	var p=req.params.cid;
-    res.render("leaderboard", { title : "Leaderboard for " + p , user_name: req.session.user_name});
+    res.render("leaderboard", { title : "Leaderboard" , user_name: req.session.user_name});
   })
   .post(function(req, res) {
 });
